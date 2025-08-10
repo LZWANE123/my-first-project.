ba8d8b2e34be79a075c3d3fd3b89b9c8093cc7d2
Linda Zwane <ZWANELINDA03@GMAIL.COM>
2025-08-10 21:39:43 +0200

    Add files via upload

---
+++ vscode101/hello.py
@@ -0,0 +1,9 @@
+def say_hello(name):
+    print("Hello, " + name)
+
+say_hello("VS Code")def say_day_of_week(date)
+def say_day_of_week(date):
+    import datetime
+    day_of_week = date.strftime("%A")
+    print("Today is " + day_of_week)
+say_day_of_week(date.today())
